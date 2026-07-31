09ac84dd34149db4848a5245f0e2e47aa9646f47
Hayqal-Hakimi <hayqal.dev@gmail.com>
2026-07-31 20:16:50 +0800

    mix_loop update

--- mix_loop.py
+++ mix_loop.py
@@ -1,15 +1,32 @@
 students = ['Amir', 'Bella', 'Chong', 'Deepa', 'Elena']
 scores = [85, 42, 90, 38, 76]
+jum_scores = len(scores)
 lulus = 0
+gagal = 0
+final_mark = []
+min_pass = int(input('sila masukkan angka lulus murid : '))
+
+purata = sum(scores) / jum_scores
 
 for murid,markah in zip(students, scores) :
     print(f'{murid}: {markah}')
 
 for markah in scores :
-    if markah >= 50 :
+    if markah >= min_pass :
         lulus += 1
 
+for markah in scores :
+    if markah < min_pass :
+        gagal += 1 
+
 print(f'\nJumlah pelajar lulus: {lulus}')
+print(f'\nJumlah pelajar gagal: {gagal}')
+print(f'\nPurata markah murid {purata}')
+
+for percent in scores :
+    percent = (percent / 140) * 100
+    final_mark.append(round(percent))
+
 
 inginCari = True
 
@@ -20,9 +37,9 @@ while inginCari :
         print('Terima Kasih')
         break
 
-    for murid,markah in zip(students, scores) : 
+    for murid,markah,markah_akhir in zip(students, scores, final_mark) : 
         if carian == murid :
-            print(f'{murid} : {markah}')
+            print(f'{murid} : {markah}\nMarkah Akhir {markah_akhir}%')
             break
     else: 
         print(f'maaf {carian} tiada dalam senarai')
