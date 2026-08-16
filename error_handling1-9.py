8ad9093fe12a5345489dfdb569acf6ccebc86579
Hayqal-Hakimi <hayqal.dev@gmail.com>
2026-08-16 22:28:33 +0800

    task belajar guna raise from e

--- error_handling1-9.py
+++ error_handling1-9.py
@@ -91,25 +91,38 @@
 # print(dapatkan_log_hari(senarai_log, 2)) 
 # print(dapatkan_log_hari(senarai_log, 5))
 
-class InsufficientFundsError(Exception):
-    def __init__(self, baki_akaun ,jumlah_keluar,) :  
-        kurang_berapa = jumlah_keluar - baki_akaun
-        self.kurang_berapa = kurang_berapa
-        self.baki_akaun = baki_akaun
-        self.jumlah_keluar = jumlah_keluar
+# class InsufficientFundsError(Exception):
+#     def __init__(self, baki_akaun ,jumlah_keluar,) :  
+#         kurang_berapa = jumlah_keluar - baki_akaun
+#         self.kurang_berapa = kurang_berapa
+#         self.baki_akaun = baki_akaun
+#         self.jumlah_keluar = jumlah_keluar
 
-def keluarkan_wang(baki_akaun, jumlah_keluar): 
-    try :
-        if baki_akaun < jumlah_keluar :
-            raise InsufficientFundsError(baki_akaun, jumlah_keluar)
-    except InsufficientFundsError as error : 
-        return f'baki tidak cukup lagi {error.kurang_berapa}'
+# def keluarkan_wang(baki_akaun, jumlah_keluar): 
+#     try :
+#         if baki_akaun < jumlah_keluar :
+#             raise InsufficientFundsError(baki_akaun, jumlah_keluar)
+#     except InsufficientFundsError as error : 
+#         return f'baki tidak cukup lagi {error.kurang_berapa}'
 
-    akaun_semasa = baki_akaun - jumlah_keluar 
-    return f'akaun semasa: {akaun_semasa}'
+#     akaun_semasa = baki_akaun - jumlah_keluar 
+#     return f'akaun semasa: {akaun_semasa}'
 
 
-print(keluarkan_wang(1000, 500))    # baki cukup, ada baki lebih - expect BERJAYA
-print(keluarkan_wang(1000, 1500))   # baki tak cukup - expect GAGAL
-print(keluarkan_wang(500, 500))     # keluar SEMUA baki - expect GAGAL (sebab akaun tak boleh 0)
+# print(keluarkan_wang(1000, 500))    # baki cukup, ada baki lebih - expect BERJAYA
+# print(keluarkan_wang(1000, 1500))   # baki tak cukup - expect GAGAL
+# print(keluarkan_wang(500, 500))     # keluar SEMUA baki - expect GAGAL (sebab akaun tak boleh 0)
     
+database = {
+    'user1': {'nama': 'Ali', 'umur': 17},
+    'user2': {'nama': 'Aina'}  # TIADA 'umur'!
+}
+
+def simpan_data(database, key):
+    try:
+        umur = database[key]['umur']
+        return umur
+    
+    except KeyError as e:
+        raise ValueError('gagal simpan data ke database') from e
+print(simpan_data(database, 'user2'))    
