51a4f1e7d6a9959a16e0c6d6b2db21d93791e21c
Hayqal-Hakimi <hayqal.dev@gmail.com>
2026-08-16 08:51:26 +0800

    task 2 error handling + tes file

--- error_handling1-9.py
+++ error_handling1-9.py
@@ -1,17 +1,39 @@
-def withdraw_amount():
-    while True:
-        mount_duit = input('masukkan angka duit yg ingin dikeluarkan : ')
-        try : 
-            mount_duit = int(mount_duit)
-        except ValueError:
-            print('sila masukkan dalam bentuk angka') 
-            continue
-        try : 
-            if mount_duit <= 0 :
-                raise ValueError('sila masukkan angka sekurang kurangnya 1')
-        except ValueError as error :
-            print(error)
-            continue
-        else : 
-            return f"duit sebanyak {mount_duit} berjaya dikeluarkan"
-print(withdraw_amount())
+# def withdraw_amount():
+#     while True:
+#         mount_duit = input('masukkan angka duit yg ingin dikeluarkan : ')
+#         try : 
+#             mount_duit = int(mount_duit)
+#         except ValueError:
+#             print('sila masukkan dalam bentuk angka') 
+#             continue
+#         try : 
+#             if mount_duit <= 0 :
+#                 raise ValueError('sila masukkan angka sekurang kurangnya 1')
+#         except ValueError as error :
+#             print(error)
+#             continue
+#         else : 
+#             return f"duit sebanyak {mount_duit} berjaya dikeluarkan"
+# print(withdraw_amount())
+
+def baca_config(nama_fail):
+    try : 
+        with open(nama_fail,'r') as openFile :
+            kandungan = openFile.read()
+            fail_yang_ambil = nama_fail
+
+    except FileNotFoundError : 
+        defult = {
+            'port' : '101:099:100',
+            'ID Server' : 'D001',
+            'IP ADDRESS' : 'B9101A'
+        }
+        kandungan = defult
+        fail_yang_ambil = 'defult'
+    finally : 
+        print(f'setting sudah dipasang meggunakana fail {fail_yang_ambil}')
+
+    return kandungan
+
+
+baca_config('tes1.txt')
