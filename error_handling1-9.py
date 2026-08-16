211a80ec66361413a9db02d2cdd7b4ee003e100c
Hayqal-Hakimi <hayqal.dev@gmail.com>
2026-08-16 10:03:00 +0800

    task3 : pakai raise untuk buat error menual dgn if dan isinstance

--- error_handling1-9.py
+++ error_handling1-9.py
@@ -16,24 +16,39 @@
 #             return f"duit sebanyak {mount_duit} berjaya dikeluarkan"
 # print(withdraw_amount())
 
-def baca_config(nama_fail):
-    try : 
-        with open(nama_fail,'r') as openFile :
-            kandungan = openFile.read()
-            fail_yang_ambil = nama_fail
+# def baca_config(nama_fail):
+#     try : 
+#         with open(nama_fail,'r') as openFile :
+#             kandungan = openFile.read()
+#             fail_yang_ambil = nama_fail
 
-    except FileNotFoundError : 
-        defult = {
-            'port' : '101:099:100',
-            'ID Server' : 'D001',
-            'IP ADDRESS' : 'B9101A'
-        }
-        kandungan = defult
-        fail_yang_ambil = 'defult'
-    finally : 
-        print(f'setting sudah dipasang meggunakana fail {fail_yang_ambil}')
+#     except FileNotFoundError : 
+#         defult = {
+#             'port' : '101:099:100',
+#             'ID Server' : 'D001',
+#             'IP ADDRESS' : 'B9101A'
+#         }
+#         kandungan = defult
+#         fail_yang_ambil = 'defult'
+#     finally : 
+#         print(f'setting sudah dipasang meggunakana fail {fail_yang_ambil}')
 
-    return kandungan
+#     return kandungan
 
 
-baca_config('tes1.txt')
+# baca_config('tes1.txt')
+
+def proses_markah(senarai_markah):
+    markah_yang_valid = []
+    for markah in senarai_markah : 
+        try : 
+            if not isinstance(markah, int) : 
+                raise TypeError(markah)
+            else : 
+                markah_yang_valid.append(markah)
+        except  TypeError as e : 
+            print(f"ankga ini tidak valid : {e}")
+            continue
+    return f'markah yang valid {markah_yang_valid}'
+
+print(proses_markah([85, '42', None, 90, 'abc']))
