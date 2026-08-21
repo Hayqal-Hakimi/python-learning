25ce2dd79f80ce379990fae425e44c0b80cdeb17
Hayqal-Hakimi <hayqal.dev@gmail.com>
2026-08-21 22:51:37 +0800

    hasil akhir siap 3 func ,tapi output masih kurang memuaskan

--- daftarPekerja.py
+++ daftarPekerja.py
@@ -3,7 +3,7 @@ import random
 senarai_pekerja_baharu = [
     {'nama': 'Ali bin Ahmad', 'email': 'ali@syarikat.com', 'umur': 25, 'jabatan': 'IT'},
     {'nama': 'Aina binti Hassan', 'email': 'aina-tiada-at.com', 'umur': 30, 'jabatan': 'HR'},
-    {'nama': 'Kumar', 'email': 'kumar@syarikat.com', 'umur': 'dua puluh', 'jabatan': 'Kewangan'},
+    {'nama': 'Kumar', 'email': 'kumar@syarikat.com', 'umur': 30, 'jabatan': 'Kewangan'},
     {'nama': 'Siti', 'email': 'siti@syarikat.com', 'umur': 28, 'jabatan': 'Pemasaran'},
 ]
 
@@ -23,6 +23,8 @@ def validate_pekerja(rekod):
     if '@' not in email : 
         raise DataTidakSahError('email', 'tidak ada @ (format penulisan email)')
     umur = rekod['umur']
+    if not isinstance(umur, int) : 
+        raise DataTidakSahError('umur', 'umur pastikan di dalam integer')
     if umur <= 18 : 
         raise DataTidakSahError('Umur', 'umur masih belum mencapai minimum syarat')
     
@@ -35,14 +37,40 @@ def daftar_ke_sistem(rekod):
     
 
 def proses_onboarding(senarai_pekerja):
-    for rekod in senarai_pekerja : 
-        validate_pekerja(rekod)
-        daftar_ke_sistem(rekod)
+    senarai_berjaya = []
+    senarai_gagal = []
+    ringkasan_sebab_gagal = []
+    for rekod in senarai_pekerja :
+        percubaan  = 0 
+        status = False
+        try : 
+            validate_pekerja(rekod)
+        except DataTidakSahError as e:
+            ringkasan_sebab_gagal.append(str(e))
+            senarai_gagal.append(rekod['nama'])
+            continue 
+        while not status and percubaan < 3 :
+            percubaan += 1
+            try : 
+                daftar_ke_sistem(rekod)
+                status = True
+                senarai_berjaya.append(f"{rekod['nama']} dgn cubaan {percubaan} kali")
+                break
+            except SambunganGagalError :
+                print(f"Percubaan ke-{percubaan} gagal untuk {rekod['nama']}. Cuba lagi...")
+            finally:
+                print(f"[LOG] Selesai proses percubaan {percubaan} untuk {rekod['nama']}")
+        else : 
+            senarai_gagal.append(rekod['nama'])
+    return f"yang berjaya : {senarai_berjaya} | jumlah yang gagal : {len(senarai_gagal)} | sebab {ringkasan_sebab_gagal}"
 
-    # untuk setiap rekod: retry MAKSIMUM 3 kali HANYA untuk SambunganGagalError
+
+
     # DataTidakSahError -> terus masuk senarai gagal, JANGAN retry
     # guna while + try + except + else + finally
     # akhir: return (senarai_berjaya, senarai_gagal, ringkasan_sebab_gagal)
     
 
 hasil = proses_onboarding(senarai_pekerja_baharu)
+
+print(hasil)
